5c03185c907294a79970211be1e93b31dc6cfeb1
irenegevyo <irenegevyo@gmail.com>
2019-02-22 16:33:03 +0700

    task 2 and 3 done

--- app/src/main/java/com/example/android/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/android/droidcafe/MainActivity.java
@@ -26,14 +26,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-/**
- * This app demonstrates images used as buttons and a floating action button
- * to use an intent to launch a second activity. The app lets a user tap an
- * image to make a choice. The app displays a Toast message showing the user’s
- * choice.
- *
- * This version demonstrates various input controls.
- */
+
 public class MainActivity extends AppCompatActivity {
 
     // Tag for the intent extra.
@@ -63,12 +56,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * Inflates the menu, and adds items to the action bar if it is present.
-     *
-     * @param menu Menu to inflate.
-     * @return Returns true if the menu inflated.
-     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -76,58 +63,43 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    /**
-     * Handles app bar item clicks.
-     *
-     * @param item Item clicked.
-     * @return True if one of the defined items was clicked.
-     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        // This comment suppresses the Android Studio warning about simplifying
-        // the return statements.
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_order) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            default:
+                // Do Nothing
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Displays a Toast with the message.
-     *
-     * @param message Message to display
-     */
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
     }
 
-    /**
-     * Shows a message that the donut image was clicked.
-     */
+
     public void showDonutOrder(View view) {
         mOrderMessage = getString(R.string.donut_order_message);
         displayToast(mOrderMessage);
     }
 
-    /**
-     * Shows a message that the ice cream sandwich image was clicked.
-     */
+
     public void showIceCreamOrder(View view) {
         mOrderMessage = getString(R.string.ice_cream_order_message);
         displayToast(mOrderMessage);
     }
 
-    /**
-     * Shows a message that the froyo image was clicked.
-     */
     public void showFroyoOrder(View view) {
         mOrderMessage = getString(R.string.froyo_order_message);
         displayToast(mOrderMessage);
